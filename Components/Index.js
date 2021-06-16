cbcfa5346682a0a1838927ab9477740ba2a9c0ef
m-mobark <36944447+m-mobark@users.noreply.github.com>
2021-06-16 23:58:03 +0200

    edit token

--- Components/Index.js
+++ Components/Index.js
@@ -16,43 +16,44 @@ const gitAllData = async () => {
 };
 gitAllData();
 
-axios(url)
-	.then((response) => {
-		const html = response.data;
-		const $ = cheerio.load(html);
-		const product = $('.product__details');
-		// console.log(product);  // git product details
-		const productInfo = [];
+axios(url).then((response) => {
+	const html = response.data;
+	const $ = cheerio.load(html);
+	const product = $('.product__details');
+	//console.log(product);  // git product details
+	const csrfToken = $('.csrftoken');
+	//console.log(csrfToken.val());
+	// git toke
 
-		product.each(function() {
-			const product_Name = $(this).find('.product__name').text();
-			const old_price = $(this).find('.price__regular').text();
-			const current_price = $(this).find('.price__current').text();
-			const discount = $(this).find('.price__discount').text();
+	const productInfo = [];
+});
+// 	product.each(function() {
+// 		const product_Name = $(this).find('.product__name').text();
+// 		const old_price = $(this).find('.price__regular').text();
+// 		const current_price = $(this).find('.price__current').text();
+// 		const discount = $(this).find('.price__discount').text();
 
-			productInfo.push({
-				name: product_Name,
-				old_price: old_price.replace(regExpression, ''),
-				current_price: current_price.replace(regExpression, ''),
-				Discount: discount.replace(regExpression, '')
-			});
-		});
+// 		productInfo.push({
+// 			name: product_Name,
+// 			old_price: old_price.replace(regExpression, ''),
+// 			current_price: current_price.replace(regExpression, ''),
+// 			Discount: discount.replace(regExpression, '')
+// 		});
+// 	});
 
-		console.log(productInfo);
-	})
-	.catch(console.error);
+// 	console.log(productInfo);
+// })
+// .catch(console.error);
 
 // ** Post data to cart **
 
-// fetch('https://www.shopdisney.co.uk/on/demandware.store/Sites-disneyuk-Site/en_GB/Cart-AddProduct', {
-// 	body:
-// 		'format=ajax&Quantity=1&pid=428411449216&csrf_token=qEZqKeTAD85buHKf2Wf2UgcEPOo-mOM3dCVxLSt4S-UNYSxOlYK0HIR6jToUvktF3_TGaQDCZuYNQ8WSZMOzE62zcG1kv6M8L89KiC9-9qSSronyk95CIF3WKfi0lZB3hcSAYun_cT7NaC7n665DfpSTyuLL25eY7HsyxoTkk-3aTzga8Eg%3D',
-// 	method: 'POST'
-// })
-// 	.then((res) => res.json())
-// 	.then((data) => {
-// 		console.log('DATA==>', data);
-// 	})
-// 	.catch((err) => {
-// 		console.log('error', err);
-// 	});
+fetch('https://www.shopdisney.co.uk/on/demandware.store/Sites-disneyuk-Site/en_GB/Cart-AddProduct', {
+	body: 'format=ajax&Quantity=1&pid=428411449216&csrf_token=${csrfToken}',
+	method: 'POST'
+})
+	.then((res) => {
+		console.log('result', res);
+	})
+	.catch((err) => {
+		console.log('error', err);
+	});
--- Components/Post.js
+++ Components/Post.js
@@ -1,15 +1,23 @@
 const fetch = require('node-fetch');
 
-fetch('https://www.shopdisney.co.uk/on/demandware.store/Sites-disneyuk-Site/en_GB/Cart-AddProduct', {
-	body:
-		'format=ajax&Quantity=1&pid=428411449216&csrf_token=mutjxjgDnd2M7NPpP1wo4pvWSEAj8qW6l4YhxOU4yUmVcBdX6N_aAel5xyMlmNR9UQ1Kw30LZA39Nf8Bkq6ITjTTWURVWbt2_eR_U9ozWaOXWMxZfWz0zfo7y0294WlfqMEn_VrnD1Bnjdjek8Igsmv_IO8DV5WviIr5HvcMsi4QX9e-wo8%3D',
-	method: 'POST',
-	mode: 'cors'
-})
-	.then((res) => {
-		console.log('result', res);
-		console.log(`statusCode: ${res.statusCode}`);
-	})
-	.catch((err) => {
-		console.log('error', err);
-	});
+const cheerio = require('cheerio');
+const html = `https://www.shopdisney.co.uk/on/demandware.store/Sites-disneyuk-Site/en_GB/Cart-AddProduct`;
+
+const $ = cheerio.load(html);
+
+let csrtToken = $('.csrftoken');
+console.log(csrtToken.val());
+
+// fetch('html', {
+// 	body:
+// 		'format=ajax&Quantity=1&pid=428411449216&csrf_token=mutjxjgDnd2M7NPpP1wo4pvWSEAj8qW6l4YhxOU4yUmVcBdX6N_aAel5xyMlmNR9UQ1Kw30LZA39Nf8Bkq6ITjTTWURVWbt2_eR_U9ozWaOXWMxZfWz0zfo7y0294WlfqMEn_VrnD1Bnjdjek8Igsmv_IO8DV5WviIr5HvcMsi4QX9e-wo8%3D',
+// 	method: 'POST',
+// 	mode: 'cors'
+// })
+// 	.then((res) => {
+// 		console.log('result', res);
+// 		console.log(`statusCode: ${res.statusCode}`);
+// 	})
+// 	.catch((err) => {
+// 		console.log('error', err);
+// 	});
